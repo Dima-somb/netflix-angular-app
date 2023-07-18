a2fd83c50c1ec48ad1d2b55c4bd8fadd51450e6f
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2023-07-18 22:26:12 +0300

    Finish with list and list-item-component.

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Input, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent {
+  trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
+  @Input() index!: number;
 
+  @ViewChild('listItemRef') listItemRef!: ElementRef;
+  isHovered = false;
+  // setIsHovered: any
+
+  setIsHovered(boolParam: boolean) {
+    if(boolParam) {
+      this.isHovered = boolParam;
+     this.listItemRef.nativeElement.style.left = `${(this.index * 225) - 50 + (this.index * 2.5)}px`;
+      console.log(this.listItemRef.nativeElement.style.left)
+    }
+  }
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -11,6 +11,7 @@ export class ListComponent {
 
   sliderNumber = 0;
   isMoved = false;
+  movieArray = [1,2,3,4,5,6,7,8,9,10];
 
   handleClick(direction: string) {
     let distance = this.listRef.nativeElement.getBoundingClientRect().x - 50;
